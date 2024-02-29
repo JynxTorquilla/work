d5e349f8790bd70a1b67a2bb70694611ce698603
Shamil <nizamov.1999@gmail.com>
2024-02-29 15:28:33 +0300

    mkdir

--- merge_fastq_2.sh
+++ merge_fastq_2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir merged_fastq_2
+
 for i in $(find ./ -type f -name "*.fastq.gz" | while read F; do basename $F | rev | cut -c 22- | rev; done | sort | uniq)
 
     do echo "Merging R1"
 
-cat "$i"_L00*_R1_001.fastq.gz > "$i"_L001_R1_001_merged.fastq.gz
+cat "$i"_L00*_R1_001.fastq.gz > ./merged_fastq_2/"$i"_L001_R1_001_merged.fastq.gz
 
 done;
